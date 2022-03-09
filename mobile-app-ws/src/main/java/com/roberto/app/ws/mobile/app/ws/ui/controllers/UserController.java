132d35ea5a4bf82f1faf4ee8718520447da69297
Carlos Roberto <carlosmedeiroslima@gmail.com>
2022-03-08 22:48:55 -0300

    Crud Implemented

--- mobile-app-ws/src/main/java/com/roberto/app/ws/mobile/app/ws/ui/controllers/UserController.java
+++ mobile-app-ws/src/main/java/com/roberto/app/ws/mobile/app/ws/ui/controllers/UserController.java
@@ -1,19 +1,33 @@
 package com.roberto.app.ws.mobile.app.ws.ui.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roberto.app.ws.mobile.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
+import com.roberto.app.ws.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.roberto.app.ws.mobile.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping(value = "users")
 public class UserController {
+	
+	private Map<String, UserRest> users;
 	
 	@GetMapping
 	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, 
@@ -22,29 +36,50 @@ public class UserController {
 		return "get users was called with page = "+page + " and limit = "+limit+ " and sort =" + sort;
 	}
 	
-	@GetMapping(path = "/{userId}")
-	public UserRest getUser(@PathVariable String userId) {
+	@GetMapping(path = "/{userId}", 
+			produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+		
+		if (users.containsKey(userId)) {
+			return new ResponseEntity<>(users.get(userId),HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		
+	}
+	
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+			     produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE }
+				)
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
 		
 		UserRest returnValue = new UserRest();
-		returnValue.setEmail("test@test.com");
-		returnValue.setFirstName("Carlos");
-		returnValue.setLastName("Lima");
-		return returnValue ;
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+		if (users == null) users = new HashMap<>();
+		users.put(userId, returnValue);
+		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK);	
 	}
 	
-	@PostMapping
-	public String createUser() {
-		return "created user was called";	
+	@PutMapping(path = "/{userId}",
+				consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+		        produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE })
+	public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+		UserRest storedUserDetails =  users.get(userId);
+		storedUserDetails.setFirstName(userDetails.getFirstName());
+		storedUserDetails.setLastName(userDetails.getLastName());
+		users.put(userId, storedUserDetails);
+		return storedUserDetails;
 	}
 	
-	@PutMapping
-	public String updateUser() {
-		return "update user was called";	
-	}
-	
-	@DeleteMapping
-	public String deleteUser() {
-		return "delete user was called";	
+	@DeleteMapping(path = "/{userId}")
+	public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+		users.remove(userId);
+		return ResponseEntity.noContent().build();	
 	}
 	
 	
